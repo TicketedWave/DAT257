fcd3bfd3bf69324a60f7fe4f5122db02a27bb72f
tild4 <101184263+tild4@users.noreply.github.com>
2025-04-09 11:10:18 +0200

    Latest

--- dat257/app/components/Footer.js
+++ dat257/app/components/Footer.js
@@ -3,7 +3,7 @@ export default function Footer() {
     <footer className="bg-black p-6">
       <div className="container mx-auto text-center">
         <p className="text-[#C1E5DF]">© {new Date().getFullYear()} CarbonCompass. All rights reserved.</p>
-        <p className="mt-2 text-[#C1E5DF]">Data sourced from World Bank</p>
+        <p className="mt-2 text-[#C1E5DF]">A project made by Chalmers students</p>
       </div>
     </footer>
   );
