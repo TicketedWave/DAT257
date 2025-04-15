23432c1b107175019ffffa92efcbfea0f0690579
Victor Jakobsson <87867368+TicketedWave@users.noreply.github.com>
2025-04-15 14:16:38 +0200

    Update page.js

--- dat257/app/map/page.js
+++ dat257/app/map/page.js
@@ -1,12 +1,15 @@
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import MapSection from '../map/components/MapSection';
+import MapComponent from '../components/MapComponent';
 
+// AIzaSyD7QbfQfs4JlKkNyPa32OQ-z3b46ctjfAw
 export default function Home() {
   return (
     <div className="flex flex-col h-screen overflow-hidden">
       <Navbar />
-      <MapSection />
+      {/* <MapSection /> */}
+      <MapComponent />
       <Footer />
     </div>
   );
