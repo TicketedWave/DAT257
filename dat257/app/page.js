12bf1098f1d41b8689a8a039118651f22e4b1005
tild4 <101184263+tild4@users.noreply.github.com>
2025-05-02 11:57:04 +0200

    fixed the right font

--- dat257/app/page.js
+++ dat257/app/page.js
@@ -2,7 +2,7 @@ import Navbar from './components/Navbar';
 import FrontSection from './components/FrontSection';
 import Footer from './components/Footer';
 
-export default function Home() {
+export default function MapPage() {
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
